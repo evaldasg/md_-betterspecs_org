baa6c8e6bb4c2778b088cb9673cfbf72ac5d8c38
Evaldas Grudzinskas <evaldas.g@icloud.com>
2016-02-12 17:32:54 +0200

    Update text with links in p elements

--- create_guide.rb
+++ create_guide.rb
@@ -6,18 +6,26 @@ doc = Nokogiri::HTML(open('/home/evaldas/rspec_guide.html'))
 File.open('rspec_guide.md', 'w') do |file|
   doc.css('article').each do |article|
     file.puts "### #{article.css('h1 a').text}\n\n"
-    article.css('p').each do |p|
-      if p.attributes['class'] && p.attributes['class'].value =~ /wrong|correct|base/
-        file.puts "```ruby"
-        file.puts "# #{p.text}"
-        p.next_element.css('pre')[0].text.split("\n").each do |line|
-          file.puts line
+    if article.css('h1 a').text == 'Automatic tests with guard'
+      article.css('p').each do |p|
+        if p.attributes['class'] && p.attributes['class'].value =~ /wrong|correct|base/
+          file.puts "```ruby"
+          file.puts "# #{p.text}"
+          p.next_element.css('pre')[0].text.split("\n").each do |line|
+            file.puts line
+          end
+          file.puts "```\n\n"
+        else
+          next if p.text =~ /Discuss this guideline|Learn more about|More about/
+          if p.elements.empty?
+            file.puts p.text.strip.gsub("\n", '').gsub(/\s\s+/, ' ')
+          else
+            if p.elements[0].name == 'a'
+              file.puts p.text.gsub(/#{p.elements.text}/, "[#{p.elements.text}](#{p.elements[0].attributes['href'].value})").strip.gsub("\n", '').gsub(/\s\s+/, ' ')
+            end
+          end
+          file.puts "\n"
         end
-        file.puts "```\n\n"
-      else
-        next if p.text =~ /Discuss this guideline|Learn more about|More about/
-        file.puts p.text.strip.gsub("\n", '').gsub(/\s\s+/, ' ')
-        file.puts "\n"
       end
     end
   end
